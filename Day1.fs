afc3b7209e18fa7dec7d493ee306e1ec6e6acf8d
Kyle D. Blocher <kyle.blocher@wagner-machine.com>
2020-12-02 10:37:12 -0600

    Day 1 part 2

--- Day1.fs
+++ Day1.fs
@@ -10,9 +10,22 @@ let getTwoEntriesSumToTarget target (data: _ seq) =
     select (e1, e2)
   }
 
-let part1 =
+let getThreeEntriesSumToTarget target (data: _ seq) =
+  query {
+    for e1 in data do
+    for e2 in data do
+    for e3 in data do
+    where (e1 + e2 + e3 = target)
+    select (e1, e2, e3)
+  }
+
+let part1 : (int * int) seq -> _ =
   Seq.head
   >> (fun (a, b) -> a * b)
+
+let part2 : (int * int * int) seq -> _ =
+  Seq.head
+  >> (fun (a, b, c) -> a * b * c)
 
 open System
 open System.IO
@@ -22,8 +35,8 @@ let run =
   "input/Day1.txt"
   |> File.ReadAllLines :> _ seq
   |> Seq.map int
-  |> getTwoEntriesSumToTarget 2020
-  |> part1
+  |> getThreeEntriesSumToTarget 2020
+  |> part2
   |> Console.Write
 
 [<EntryPoint>]
