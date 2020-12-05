ff068c85a7466b31b3790e860d15609f4e9a92e4
Kyle D. Blocher <kyle.blocher@wagner-machine.com>
2020-12-05 12:34:21 -0600

    Day 3 part 2

--- Day3.fs
+++ Day3.fs
@@ -15,10 +15,20 @@ let encounter pos (line: _ array) =
   line.[pos % line.Length] = Tree
 
 let checkSlope right down =
-  Seq.mapi (fun i -> encounter (i * right))
-  >> Seq.mapi (fun i x -> i, x)
-  >> Seq.where (fun (i, x) -> x && i % down = 0)
+  Seq.mapi (fun i x -> i, x)
+  >> Seq.where (fun (i, x) -> i % down = 0 && encounter ((i / down) * right) x)
   >> Seq.length
+
+let part1 lines =
+  lines |> checkSlope 3 1
+
+open System.IO
+open System
+let part2 lines =
+  let lines = lines |> Seq.cache
+  [1, 1; 3, 1; 5, 1; 7, 1; 1, 2]
+  |> Seq.map (fun (a, b) -> checkSlope a b lines)
+  |> Seq.fold (*) 1
 
 open System
 open System.IO
@@ -28,7 +38,7 @@ let run =
   "input/Day3.txt"
   |> File.ReadAllLines :> _ seq
   |> Seq.map (Seq.map parse >> Seq.toArray)
-  |> checkSlope 3 1
+  |> part2
   |> Console.WriteLine
 
 [<EntryPoint>]
