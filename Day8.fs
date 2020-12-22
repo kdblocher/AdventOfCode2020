ac62b0195763d8c85556cc10722c001efbc84159
Kyle Blocher <kyle.blocher@wagner-machine.com>
2020-12-22 14:49:56 -0600

    Day 8 part 2

--- Day8.fs
+++ Day8.fs
@@ -1,5 +1,15 @@
 module Day8
 
+module Seq =
+  open FSharpPlus
+  let foldWhile folder state =
+    Some state
+    |> Seq.scan (fun state e -> 
+      state >>= (fun state -> folder state e))
+    >> Seq.takeWhile Option.isSome
+    >> Seq.last
+    >> Option.get
+
 type Instruction =
   | NoOperation
   | Accumulate
@@ -15,20 +25,11 @@ type LineResult = {
   Offset: int
 }
 
-type ProgramResult = {
+type ProgramState = {
   Accumulator: int
   Counter: int
+  Terminated: bool
 }
-
-module Seq =
-  open FSharpPlus
-  let foldWhile folder state =
-    Some state
-    |> Seq.scan (fun state e -> 
-      state >>= (fun state -> folder state e))
-    >> Seq.takeWhile Option.isSome
-    >> Seq.last
-    >> Option.get
 
 let parseInstruction =
   function
@@ -44,11 +45,14 @@ let executeOnce { Instruction = instruction; Operand = operand } acc =
   | Jump -> { Accumulator = acc; Offset = operand }
 
 let execute (lines: _ array) =
-  { Accumulator = 0; Counter = 0 }
+  { Accumulator = 0; Counter = 0; Terminated = false }
   |> Seq.unfold (fun state ->
-    let lineResult = executeOnce lines.[state.Counter] state.Accumulator
-    let state' = { Accumulator = lineResult.Accumulator; Counter = lineResult.Offset + state.Counter }
-    Some (state, state'))
+    if state.Terminated then Some (state, state)
+    else
+      let lineResult = executeOnce lines.[state.Counter] state.Accumulator
+      let counter = lineResult.Offset + state.Counter
+      let state' = { Accumulator = lineResult.Accumulator; Counter = counter; Terminated = counter = lines.Length }
+      Some (state, state'))
 
 let executeUntil lines =
   execute lines
@@ -61,6 +65,24 @@ let executeUntil lines =
 
 let part1 =
   executeUntil >> (fun result -> result.Accumulator)
+
+let swapInstruction = function
+  | Jump -> NoOperation
+  | NoOperation -> Jump
+  | x -> x
+
+let gen lines =
+  lines
+  |> Seq.mapi (fun i line ->
+    let lines' = lines |> Array.copy
+    lines'.[i] <- { line with Instruction = swapInstruction line.Instruction }
+    lines')
+
+let part2 =
+  gen
+  >> Seq.map executeUntil
+  >> Seq.find (fun s -> s.Terminated)
+  >> fun s -> s.Accumulator
 
 open System.Text.RegularExpressions
 let parseLine input =
@@ -77,5 +99,5 @@ let run () =
   "input/Day8.txt"
   |> File.ReadAllLines
   |> Array.map parseLine
-  |> part1
+  |> part2
   |> Console.WriteLine
