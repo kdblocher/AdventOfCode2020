9bb2e4af4e2ad8d0650f1565a054fa2b4300921f
Kyle Blocher <kdblocher@gmail.com>
2020-12-23 15:49:09 -0600

    Day 12 part 2

--- Day12.fs
+++ Day12.fs
@@ -5,56 +5,95 @@ type Direction =
   | East
   | South
   | West
-let getVector value =
-  function
-  | North -> -value, 0
-  | East -> 0, +value
-  | South -> +value, 0
-  | West -> 0, -value
 
 let directions = [| North; East; South; West |]
+
+let getVector value =
+  function
+  | North -> 0, +value
+  | East -> +value, 0
+  | South -> 0, -value
+  | West -> -value, 0
 
 type Rotation =
   | Left
   | Right
 
-open FSharpPlus.Math.Generic
-let rotate rotation angle direction =
-  let binOp = match rotation with Left -> (-) | Right -> (+)
-  let index = remE (binOp (Array.findIndex ((=) direction) directions) (angle / 90)) directions.Length
-  index |> Array.get directions
-
 type Transposition =
   | Forward
 
-open FSharpPlus
-
-let combine (x1, y1) (x2, y2) =
-  (x1 + x2), (y1 + y2)
-
-type Position = {
-  Location: int * int
-  Direction: Direction
-}
-let move pos value direction =
-  { pos with Location = combine pos.Location (getVector value direction) }
+module Tuple =
+  let inline map op (x1, y1) (x2, y2) =
+    op x1 x2, op y1 y2
 
 type Action =
   | Cardinal of Direction
   | Move of Transposition
   | Rotate of Rotation
 
-let actOnce pos value =
-  function
-  | Cardinal d -> move pos value d
-  | Move m -> move pos value pos.Direction
-  | Rotate r -> { pos with Direction = rotate r value pos.Direction }
+module Part1 =
 
-let act =
-  Seq.fold (fun pos (action, value) -> actOnce pos value action) { Location = 0, 0; Direction = East }
+  type Position = {
+    Location: int * int
+    Direction: Direction
+  }
 
-let part1 =
-  act >> (fun { Location = (a, b) } -> a + b )
+  let moveShip pos value direction =
+    { pos with Location = Tuple.map (+) pos.Location (getVector value direction) }
+
+  open FSharpPlus.Math.Generic
+  let rotateShip rotation angle direction =
+    let binOp = match rotation with Left -> (-) | Right -> (+)
+    let index = remE (binOp (Array.findIndex ((=) direction) directions) (angle / 90)) directions.Length
+    index |> Array.get directions
+
+  let actOnce pos value =
+    function
+    | Cardinal d -> moveShip pos value d
+    | Move m -> moveShip pos value pos.Direction
+    | Rotate r -> { pos with Direction = rotateShip r value pos.Direction }
+
+  let act : _ seq -> Position =
+    Seq.fold (fun pos (action, value) -> actOnce pos value action) { Location = 0, 0; Direction = East }
+
+let part1 : _ seq -> int =
+  Part1.act >> (fun { Location = (a, b) } -> abs a + abs b )
+
+module Part2 =
+
+  type Position = {
+    Location: int * int
+    Waypoint: int * int
+  }
+
+  let moveWaypoint pos value direction =
+    { pos with Waypoint = Tuple.map (+) pos.Waypoint (getVector value direction) }
+  let moveShip pos value = 
+    let vectorWaypoint = Tuple.map (*) (value, value) pos.Waypoint
+    { pos with Location = Tuple.map (+) pos.Location vectorWaypoint }
+
+  open FSharpPlus.Math.Generic
+  let rotateWaypoint rotation angle (x, y) =
+    let unOp = match rotation with Left -> (-) 0 | Right -> (+) 0
+    let index = remE (unOp (angle / 90)) directions.Length
+    match index with
+    | 0 -> +x, +y
+    | 1 -> +y, -x
+    | 2 -> -x, -y
+    | 3 -> -y, +x
+    | _ -> failwith "Unexpected rotation"
+
+  let actOnce pos value =
+    function
+    | Cardinal d -> moveWaypoint pos value d
+    | Move m -> moveShip pos value
+    | Rotate r -> { pos with Waypoint = rotateWaypoint r value pos.Waypoint }
+
+  let act : _ seq -> Position =
+    Seq.fold (fun pos (action, value) -> actOnce pos value action) { Location = 0, 0; Waypoint = 10, 1 }
+
+let part2 : _ seq -> int =
+  Part2.act >> (fun { Location = (a, b) } -> abs a + abs b )
 
 let parseAction =
   function
@@ -80,5 +119,5 @@ let run () =
   "input/Day12.txt"
   |> File.ReadAllLines
   |> Seq.map parseLine
-  |> part1
+  |> part2
   |> Console.WriteLine
