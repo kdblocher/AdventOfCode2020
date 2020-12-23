c4c3f6a363dbfe44e7e244c12444c78f4011aff2
Kyle Blocher <kdblocher@gmail.com>
2020-12-23 14:00:03 -0600

    Day 11 part 2

--- Day11.fs
+++ Day11.fs
@@ -1,12 +1,43 @@
 module Day11
 
 type Location =
-  | Floor
-  | Empty
-  | Occupied
+| Floor
+| Empty
+| Occupied
 let occupied = function Occupied -> true | _ -> false
 
-let adjacent (area: _ array array) x y =
+let genOnce adjacent tolerance area =
+  let mutable changed = false
+  area
+  |> Array.mapi (fun x ->
+    Array.mapi (fun y ->
+      function
+      | Floor -> Floor
+      | Empty ->
+        if adjacent area x y |> Seq.exists occupied |> not
+        then changed <- true; Occupied
+        else Empty
+      | Occupied ->
+        if adjacent area x y |> Seq.filter occupied |> Seq.length >= tolerance
+        then changed <- true; Empty
+        else Occupied
+    ))
+  |> fun area -> if changed then Some area else None
+
+let gen genOnce area =
+  let rec loop area =
+    match genOnce area with
+    | Some area -> loop area
+    | None -> area
+  loop area
+
+let solve adjacent tolerance =
+  gen (genOnce adjacent tolerance)
+  >> Seq.collect id
+  >> Seq.filter occupied
+  >> Seq.length
+
+let immediateAdjacent (area: _ array array) x y =
   seq {
     if (x > 0) then
       if y > 0 then yield area.[x - 1].[y - 1]
@@ -20,35 +51,33 @@ let adjacent (area: _ array array) x y =
       if y < area.[x + 1].Length - 1 then yield area.[x + 1].[y + 1]
   }
 
-let genOnce area =
-  let mutable changed = false
-  area
-  |> Array.mapi (fun x ->
-    Array.mapi (fun y ->
-      function
-      | Floor -> Floor
-      | Empty ->
-        if adjacent area x y |> Seq.exists occupied |> not
-        then changed <- true; Occupied
-        else Empty
-      | Occupied ->
-        if adjacent area x y |> Seq.filter occupied |> Seq.length >= 4
-        then changed <- true; Empty
-        else Occupied
-    ))
-  |> fun area -> if changed then Some area else None
-
-open FSharpPlus
-let rec gen area =
-  match genOnce area with
-  | Some area -> gen area
-  | None -> area
-
 let part1 =
-  gen
-  >> Seq.collect id
-  >> Seq.filter occupied
-  >> Seq.length
+  solve immediateAdjacent 4
+
+let octoAdjacent (area: _ array array) x y =
+  let rec look x y dx dy =
+    let x = x + dx
+    let y = y + dy
+    if x < 0 || y < 0 || x >= area.Length || y >= area.[x].Length
+    then None
+    else
+      match area.[x].[y] with
+      | Floor -> look x y dx dy
+      | x -> Some x
+  seq {
+    look x y -1 -1
+    look x y -1 +0
+    look x y -1 +1
+    look x y +0 -1
+    look x y +0 +1
+    look x y +1 -1
+    look x y +1 +0
+    look x y +1 +1
+  }
+  |> Seq.choose id
+
+let part2 =
+  solve octoAdjacent 5
 
 let parseChar =
   function
@@ -71,5 +100,5 @@ let run () =
   "input/Day11.txt"
   |> File.ReadAllLines
   |> parse
-  |> part1
+  |> part2
   |> Console.WriteLine
