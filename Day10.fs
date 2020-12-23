135d40dd60d5850cfb67f7d2bc4fedba639e0f6a
Kyle Blocher <kdblocher@gmail.com>
2020-12-23 12:42:01 -0600

    Day 10 part 2

--- Day10.fs
+++ Day10.fs
@@ -1,18 +1,32 @@
 module Day10
 
-let part1 =
-  Seq.append (Seq.singleton 0) // charging outlet
-  >> Seq.sort
-  >> Seq.toArray
-  >> Seq.pairwise
+let part1 : int seq -> int =
+  Seq.pairwise
   >> Seq.map (fun (a, b) -> b - a)
   >> Seq.groupBy id
   >> Seq.map (fun (a, b) -> a, Seq.length b)
   >> Map
-  >> (fun m ->
-    Map.find 1 m
-    * (Map.find 3 m
-      + 1)) // built-in adapter
+  >> (fun m -> Map.find 1 m * Map.find 3 m)
+  
+let part2 input =
+  input
+  |> Seq.skip 1
+  |> Seq.fold (fun m a ->
+    m |> Map.add a (
+      seq { 1 .. 3 }
+      |> Seq.choose (fun b -> Map.tryFind (a - b) m)
+      |> Seq.sum))
+    (Map.empty |> Map.add 0 1L)
+  |> Map.find (input |> Array.last)
+
+let prep =
+  Array.sort
+  >> (fun input ->
+  [|
+    Array.singleton 0
+    input
+    input |> Array.last |> (+) 3 |> Array.singleton
+  |] |> Array.concat)
 
 let parse =
   Array.map int
@@ -23,5 +37,6 @@ let run () =
   "input/Day10.txt"
   |> File.ReadAllLines
   |> parse
-  |> part1
+  |> prep
+  |> part2
   |> Console.WriteLine
