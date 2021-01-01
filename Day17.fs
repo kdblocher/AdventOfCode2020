6db0c2ca81ffea535a7f02602644cbf92705cdac
Kyle Blocher <kdblocher@gmail.com>
2021-01-01 12:46:58 -0600

    Day 17 part 2

--- Day17.fs
+++ Day17.fs
@@ -1,44 +1,52 @@
 module Day17
 open System
 
-let init =
-  Seq.mapi (fun x ->
-    Seq.mapi (fun y ->
-      Seq.mapi (fun z c ->
-        c, [x; y; z])))
-  >> Seq.collect id
-  >> Seq.collect id
-  >> Seq.choose (fun (cube, pos) -> if cube then Some pos else None)
-  >> set
+module List =
+  let cons a b = a :: b
 
 let update count active =
   (active && (count = 2 || count = 3))
-  || (not active && count = 3) 
+  || (not active && count = 3)
+
+let product bounds =
+  Seq.foldBack (fun (a, b) s ->
+    seq { a .. b }
+    |> (if Seq.isEmpty s
+      then Seq.map List.singleton
+      else Seq.allPairs s >> Seq.map (fun (ls, x) -> List.cons x ls))) bounds Seq.empty
 
 let cycleOnce active =
+  let dim =
+    active
+    |> Seq.head
+    |> List.length
+
   let bound i = 
     active
     |> Set.toSeq
     |> Seq.map (List.skip i >> List.head)
-    |> (fun d -> Seq.min d, Seq.max d) 
-  let x0, x' = bound 0
-  let y0, y' = bound 1
-  let z0, z' = bound 2
+    |> fun d -> Seq.min d, Seq.max d
 
-  seq { x0 - 1 .. x' - x0 + 2 } |> Seq.collect (fun x ->
-    seq { y0 - 1 .. y' - z0 + 2 } |> Seq.collect (fun y ->
-      seq { z0 - 1 .. z' - z0 + 2 } |> Seq.map (fun z ->
-        let pos = [x; y; z]
-        let count =
-          seq { -1 .. 1 } |> Seq.collect (fun dx ->
-            seq { -1 .. 1 } |> Seq.collect (fun dy ->
-              seq { -1 .. 1 } |> Seq.map (fun dz ->
-                [dx; dy; dz])))
-          |> Seq.filter ((<>) [0; 0; 0])
-          |> Seq.map (List.map2 (+) pos)
-          |> Seq.filter (fun d -> active |> Set.contains d)
-          |> Seq.length
-        pos, count)))
+  let bounds =
+    List.init dim bound
+    |> Seq.map (fun (a, b) -> a - 1, b - a + 2)
+    |> product
+  
+  let neighbors =
+    List.init dim (fun _ -> -1, 1)
+    |> product
+    |> Seq.filter (not << List.forall ((=) 0))
+  
+  let counts =
+    bounds
+    |> Seq.map (fun pos ->
+      pos,
+      neighbors
+      |> Seq.map (List.map2 (+) pos)
+      |> Seq.filter (fun d -> active |> Set.contains d)
+      |> Seq.length)
+  
+  counts
   |> Seq.fold (fun active' (pos, count) ->
     if active |> Set.contains pos |> update count
     then Set.add pos active'
@@ -50,8 +58,17 @@ let cycle count =
     if i = count then active else loop (i + 1) (cycleOnce active)
   loop 0
 
-let part1 =
-  init
+let init dim =
+  Seq.map (fun pos -> List.append pos (List.init (dim - 2) (fun _ -> 0)))
+  >> set
+
+let part1 : _ seq -> _ =
+  init 3
+  >> cycle 6
+  >> Set.count
+
+let part2 : _ seq -> _ =
+  init 4
   >> cycle 6
   >> Set.count
 
@@ -62,15 +79,20 @@ let parseChar =
   | _ -> failwith "Invalid cube character"
 
 let parseLine : _ seq -> _ =
-  Seq.map (parseChar >> Seq.singleton)
+  Seq.map parseChar
 
-let parse : _ seq -> _ =
+let parse =
   Seq.map parseLine
+  >> Seq.mapi (fun x ->
+    Seq.mapi (fun y c ->
+      c, [x; y]))
+  >> Seq.collect id
+  >> Seq.choose (fun (cube, pos) -> if cube then Some pos else None)
  
 open System.IO
 let run () =
   "input/Day17.txt"
   |> File.ReadAllLines
   |> parse
-  |> part1
+  |> part2
   |> Console.WriteLine
