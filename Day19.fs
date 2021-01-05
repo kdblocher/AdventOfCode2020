7f6c749de9d1d7be87ae523ed78734aa4f884f89
Kyle Blocher <kdblocher@gmail.com>
2021-01-05 14:52:59 -0600

    Day 19 part 2

--- Day19.fs
+++ Day19.fs
@@ -1,59 +1,56 @@
 module Day19
+open FSharpx.Collections
 
 type Id = int
 type Rule =
   | Character of char
-  | Sequence of Rule list
-  | Disjunction of Rule * Rule
+  | Sequence of Rule LazyList
+  | Disjunction of Rule LazyList
 
 type Rules = (Id, Rule) Map
 
 type CheckResult =
-  | Succ
-  | Fail
+  | Success
   | More of Rule
 
-let rec check pos char =
-  function
-  | Character c ->
-    if char = c then Succ else Fail
-  | Disjunction (a, b) ->
-    let f = check pos char
-    match f a, f b with
-    | Fail, Fail        -> Fail
-    | Fail, r | r, Fail -> r
-    | Succ, _ | _, Succ -> Succ
-    | More a, More b ->
-      More (Disjunction (a, b))
-  | Sequence [] ->
-    Succ
-  | Sequence [rule] ->
-    check pos char rule
-  | Sequence (rule :: rest) ->
-    match check pos char rule with
-    | Fail -> Fail
-    | Succ -> More (Sequence rest)
-    | More r -> More (Sequence (r :: rest))
+let rec check char rule =
+  seq {
+    match rule with
+    | Character c ->
+      if char = c then yield Success
+    | Sequence none when none.IsEmpty ->
+      yield Success
+    | Sequence rule when rule.Tail.IsEmpty ->
+      yield! check char rule.Head
+    | Sequence rules ->
+      yield! check char rules.Head
+      |> Seq.map (
+        function
+        | Success -> More (Sequence rules.Tail)
+        | More r -> More (Sequence (rules.Tail |> LazyList.cons r)))
+    | Disjunction rules ->
+      yield! rules |> Seq.collect (check char)
+  }
 
 let isMatch rule =
-  let rec loop pos rule =
+  let rec loop results =
     function
-    | [] -> rule = Succ
+    | [] ->
+      results |> Seq.contains Success
     | char :: rest ->
-      match rule with
-      | More rule ->
-        let result = check pos char rule
-        loop (pos + 1) result rest
-      | _ -> false
-  loop 0 (More rule)
+      results |> Seq.exists (
+        function
+        | More rule -> loop (check char rule) rest
+        | _ -> false)
+  loop (Seq.singleton (More rule))
 
 module Parse =
   open FSharpPlus
 
-  type Def =
+  type Definition =
     | Rule of Rule
     | RefSequence of int list
-    | DefDisjunction of Def * Def
+    | DefDisjunction of Definition * Definition
 
   let rec walk env =
     let find r = Map.find r env
@@ -61,12 +58,13 @@ module Parse =
     | Rule x ->
       x
     | DefDisjunction (r1, r2) ->
-      Disjunction (walk env r1, walk env r2)
+      Disjunction (seq { walk env r1; walk env r2 } |> LazyList.ofSeq)
     | RefSequence [ref] ->
       find ref |> walk env
     | RefSequence refs ->
       refs
-      |> List.map (find >> walk env)
+      |> Seq.map (find >> walk env)
+      |> LazyList.ofSeq
       |> Sequence
 
   let parseSequence =
@@ -100,7 +98,7 @@ module Parse =
     Regex.Split (sets.[0], "\r\n") |> parseRules,
     Regex.Split (sets.[1], "\r\n") |> Seq.map parseMessage
 
-let part1 (rule, messages) =
+let solve (rule, messages) =
   messages
   |> Seq.filter (isMatch rule)
   |> Seq.length
@@ -110,5 +108,5 @@ open System.IO
 let run () =
   File.ReadAllText "input/Day19.txt"
   |> Parse.parse
-  |> part1
+  |> solve
   |> Console.WriteLine
